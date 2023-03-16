db0b1e806e8a2ea6bea7d10cc0eb2b8211b0cc78
ProphetFlark <choper.paiz@gmail.com>
2023-03-15 21:36:28 -0600

    CSRF_TRUSTED_ORIGINS

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -128,6 +128,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = ['https://ecommerce-crp-django.up.railway.app']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
